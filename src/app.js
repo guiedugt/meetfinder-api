68e4aa69b18137b41dd73a2047790947207e86bc
Guilherme Eduardo Gulinoski Teixeira <guilherme-teixeira@guilherme.teixeira>
2019-05-23 18:20:01 -0300

    Refactor app

--- src/app.js
+++ src/app.js
@@ -7,30 +7,24 @@ const express = require('express');
 const configureDocs = require('./docs');
 const configureRoutes = require('./routes');
 const mongodb = require('./db/mongodb');
+const cors = require('./middleware/cors');
 const logger = require('./middleware/logger');
 const notFound = require('./middleware/notFound');
 const serverError = require('./middleware/serverError');
 
 const app = express();
-
 const port = process.env.PORT || '3000';
-
-app.set('port', port);
-
-app.use('/', logger);
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use((_req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
-  next();
-});
 
 configureDocs(app);
 configureRoutes(app);
 
+app.set('port', port);
+
+app.use(logger);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(cors);
 app.use(mongodb.connect);
 app.use(notFound);
 app.use(serverError);
